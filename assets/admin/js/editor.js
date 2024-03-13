781aab2b62333f4ca6da6fbfb41124078bd29311
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-03-13 17:53:03 +0200

    Create editor.js

---
+++ assets/admin/js/editor.js
@@ -0,0 +1,14 @@
+(function(wp){
+  wp.domReady(function() {
+    const blockNames = ['core/group'];
+    const allowedBlocks = wp.blocks.getBlockTypes().filter(block => blockNames.includes(block.name));
+
+    window.addEventListener('load', event => {
+      allowedBlocks.forEach(block => {
+        if (! wp.blocks.getBlockType(block.name)) {
+          wp.blocks.registerBlockType(block.name, block);
+        }
+      });
+    });
+  });
+})(window.wp);

3348f5a3c3f2c024432460d811b834ddd0de4854
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-06-26 15:52:56 +0300

    Update helsinki-testbed-core.php

--- helsinki-testbed-core.php
+++ helsinki-testbed-core.php
@@ -1,140 +1,56 @@
 <?php
-/*
-Plugin Name:  Helsinki Testbed Core
-Plugin URI:   https://genero.fi
-Description:  Register Post Types and Taxonomies for site
-Version:      2.1.0
-Author:       Genero
-Author URI:   https://genero.fi/
-License:      MIT License
-*/
+/**
+	* Plugin Name: Helsinki Testbed Core
+	* Description: Site specific features
+	* Requires at least: 6.0
+	* Requires PHP: 7.4
+	* Version: 3.0.0
+	* Author: ArtCloud
+	* Author URI: https://www.artcloud.fi
+	* License: MIT License
+	* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+	* Text Domain: helsinki-testbed-core
+	* Domain Path: /languages
+	*/
 
-namespace Genero\Site;
+declare(strict_types = 1);
 
-if (!is_blog_installed()) {
-    return;
+namespace CityOfHelsinki\WordPress\Testbed\Core;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
 /**
- * Register custom post types and taxonomies with WP.
- *
- * @see https://github.com/jjgrainger/PostTypes
- * @see https://developer.wordpress.org/resource/dashicons/
- */
-class PostTypes
-{
-    private static $instance = null;
+  * Setup
+  */
+require_once plugin_dir_path( __FILE__ ) . 'constants.php';
+define_constants( __FILE__ );
 
-    public static function getInstance()
-    {
-        if (null === self::$instance) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-    }
+require_once plugin_dir_path( __FILE__ ) . 'functions.php';
+load_includes();
 
-    /**
-     * Register all post types and their taxonomies.
-     * @note needs to run during `init`.
-     */
-    public function register()
-    {
-        $this->registerPost();
-        $this->registerPage();
-        $this->registerPerson();
-    }
+spl_autoload_register( __NAMESPACE__ . '\\class_loader' );
 
-    public function registerPost()
-    {
-        // Set `has_archive` for compatibility with `post-type-archive-mapping`.
-        add_filter('register_post_type_args', function ($args, $post_type) {
-            if ($post_type === 'post') {
-                $args['has_archive'] = true;
-                $args['rewrite'] = [
-                    'with_front' => true,
-                ];
-            }
-            return $args;
-        }, 10, 2);
-    }
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\loaded' );
 
-    public function registerPage()
-    {
-        add_action('init', function () {
-            add_post_type_support('page', 'excerpt');
-        });
-    }
+/**
+  * Init
+  */
+add_action( 'init', __NAMESPACE__ . '\\init', 100 );
+add_action( 'helsinki_testbed_core_init', __NAMESPACE__ . '\\textdomain' );
 
-    public function registerPerson()
-    {
-		add_action('init', function () {
-            register_post_type('person', array(
-				'labels' => array(
-					'name' => 'Persons',
-					'singular_name' => 'Person',
-				),
-				'public' => false,
-	            'show_ui' => true,
-	            'has_archive' => false,
-	            'show_in_rest' => true,
-	            'supports' => ['title', 'thumbnail'],
-				'menu_icon' => 'dashicons-admin-users',
-			));
-        });
+/**
+  * Activation
+  */
+// register_activation_hook( __FILE__, __NAMESPACE__ . '\\activate' );
 
-		add_filter('manage_person_posts_columns', function($columns){
-			$sorted = array();
-			foreach ($columns as $key => $value) {
-				$sorted[$key] = $value;
+/**
+  * Deactivation
+  */
+// register_deactivation_hook( __FILE__, __NAMESPACE__ . '\\deactivate' );
 
-				if ( 'cb' === $key ) {
-					$sorted['thumbnail'] = '';
-				}
-			}
-
-			return $sorted;
-		});
-
-		add_action('manage_person_posts_custom_column', function($column, $post_id){
-			if ('thumbnail' === $column) {
-				echo get_the_post_thumbnail($post_id, 'thumbnail');
-			}
-		}, 10, 2);
-    }
-
-    public function adminHead()
-    {
-        echo '<style>
-            .wp-list-table th.column-thumbnail { width: 28px; }
-            .wp-list-table td.column-thumbnail img {
-                max-width: 37px;
-                max-height: 37px;
-                width: auto;
-                height: auto;
-            }
-        </style>';
-    }
-}
-
-add_action( 'plugins_loaded', __NAMESPACE__ . '\\init_plugin', 9 );
-function init_plugin() {
-	$plugin = PostTypes::getInstance();
-
-	$plugin->register();
-
-	add_action('admin_head', [$plugin, 'adminHead']);
-
-	add_filter( 'helsinki_wp_disallowed_blocks', __NAMESPACE__ . '\\filter_helsinki_wp_disallowed_blocks' );
-}
-
-function filter_helsinki_wp_disallowed_blocks( array $disallowed ): array {
-	if ( isset( $disallowed['common']['core/media-text'] ) ) {
-		unset( $disallowed['common']['core/media-text'] );
-	}
-
-	if ( isset( $disallowed['post_types']['post']['core/group'] ) ) {
-		unset( $disallowed['post_types']['post']['core/group'] );
-	}
-
-	return $disallowed;
-}
+/**
+  * Uninstall
+  */
+// register_uninstall_hook( __FILE__, __NAMESPACE__ . '\\uninstall' );

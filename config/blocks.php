0f07b93714aa30c8a8587573ff6a5a6b9579186b
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-08-06 17:34:18 +0300

    Create blocks.php

---
+++ config/blocks.php
@@ -0,0 +1,14 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+return [
+	'content-list' => [
+		'render_callback' => 'ContentList\render',
+	],
+    // 'highlight' => [],
+    // 'icon-and-text' => [],
+    // 'person-list' => [],
+];

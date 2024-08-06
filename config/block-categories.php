9e46cdc45a2040a3f78fdc15e4cbf792260edd94
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-08-06 17:34:16 +0300

    Create block-categories.php

---
+++ config/block-categories.php
@@ -0,0 +1,12 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+return array(
+	array(
+		'slug'  => 'helsinki-testbed-core',
+		'title' => __( 'Testbed Helsinki', 'helsinki-testbed-core' ),
+	)
+);

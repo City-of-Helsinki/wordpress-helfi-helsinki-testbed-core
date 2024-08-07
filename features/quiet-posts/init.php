3843290ee4533d4c07604bc7717874473f38b19f
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-08-07 12:33:35 +0300

    Update init.php

--- features/quiet-posts/init.php
+++ features/quiet-posts/init.php
@@ -12,4 +12,6 @@ add_action( 'helsinki_testbed_core_loaded', __NAMESPACE__ . '\\init' );
 function init(): void {
 	add_action( 'add_meta_boxes_post', __NAMESPACE__ . '\\register_custom_meta_box' );
 	add_action( 'save_post_post', __NAMESPACE__ . '\\save_custom_post_meta', 10, 3 );
+
+	add_filter( 'helsinki_testbed_core_quiet_post_meta_key', __NAMESPACE__ . '\\quiet_post_meta_key' );
 }

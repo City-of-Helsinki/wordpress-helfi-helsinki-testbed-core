34893df1475bb9afb72db16c5601d3a9e9b92103
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-08-09 13:12:59 +0300

    searchwp

--- integrations/searchwp-live-ajax-search/assets/scripts.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  jQuery( document ).on(
-    'searchwp_live_search_success',
-    function ( event, $input, $results, $form, action, values ) {
-      // TODO:
-    }
-  );
-})();
--- integrations/searchwp-live-ajax-search/init.php
+++ integrations/searchwp-live-ajax-search/init.php
@@ -11,7 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action( 'helsinki_testbed_core_loaded', __NAMESPACE__ . '\\init' );
 function init(): void {
 	add_filter( 'searchwp_live_search_results_template', __NAMESPACE__ . '\\custom_search_results_template_path' );
-	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_assets', 1 );
 }
 
 function custom_search_results_template_path( string $template ): string {
@@ -22,19 +21,6 @@ function custom_search_results_template_path( string $template ): string {
 			'searchwp-live-ajax-search',
 			'templates',
 			'search-results',
-		)
-	);
-}
-
-function enqueue_assets(): void {
-	wp_enqueue_script(
-		'testbed-searchwp-live-ajax-search',
-		plugin_dir_url( __FILE__ ) . 'assets/scripts.js',
-		array( 'swp-live-search-client' ),
-		apply_filters( 'helsinki_testbed_core_asset_version', '' ),
-		array(
-			'strategy' => 'async',
-			'in_footer' => true,
 		)
 	);
 }
